b1006fffb17f7773b796a40bcddd1958dbf36995
PauloGit00 <paulogithub22@gmail.com>
2025-11-03 14:35:41 -0300

    adicionei menu burguer

--- script.js
+++ script.js
@@ -45,4 +45,21 @@ document.addEventListener("DOMContentLoaded", () => {
             botao.style.boxShadow ="none";
         });
     });
+
 });
+
+    function clickMenu() {
+        if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+        } else {
+        itens.style.display = 'block'
+        }
+    }
+
+    function mudouTamanho() {
+        if (window.innerWidth >=768) {
+            itens.style.display ='block'
+        } else {
+            itens.style.display ='none'
+        }
+    }
